12d741834a39cb8659720c202dc68b91b178e7e2
2021020660-Yunkai Cui <1529083432@qq.com>
2022-02-28 23:53:30 +0800

    Config the Kconfig and set the CONFIG_WATCHPOINT to optimize the programe when WP is unnecessary.

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -26,12 +26,17 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) log_write("%s\n", _this->logbuf);
 #endif
+
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+
+#ifdef CONFIG_WATCHPOINT
   if(is_wp_change() == 1){
     printf("Programe stoped because of the change of watchpoint!\n");
     nemu_state.state = NEMU_STOP;
   }
+#endif
+
 }
 
 #include <isa-exec.h>
