61eb7005240651848380a415c495bdac19dd2f5c
2021020660-Yunkai Cui <1529083432@qq.com>
2022-03-23 19:59:06 +0800

    Finish the necessary instructions of dummy.c! It is the first step!

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -2,11 +2,21 @@ def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
 }
 
-def_EHelper(li){
-  rtl_li(s, ddest, id_src1->simm);
+def_EHelper(addi){
+  rtl_addi(s, ddest, dsrc1, id_src2->simm);
+  printf("after_addi:0x%08x\n", *ddest);
 }
 
 def_EHelper(auipc){
   rtl_auipc(s, ddest, id_src1->simm);
 }
+
+def_EHelper(jal){
+  rtl_jal(s, ddest, id_src1->simm);
+}
+
+def_EHelper(jalr){
+  rtl_jalr(s, ddest, dsrc1, id_src2->simm);
+}
+
 
